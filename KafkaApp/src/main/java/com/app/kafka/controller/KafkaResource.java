a8622386bd2feb404fd2d539a84880db61424651
nilangekar-ni3 <nilangekar.ni3@gmail.com>
2022-08-04 12:51:53 +0530

    sending Message to Kafka Topic

--- KafkaApp/src/main/java/com/app/kafka/controller/KafkaResource.java
+++ KafkaApp/src/main/java/com/app/kafka/controller/KafkaResource.java
@@ -1,17 +1,25 @@
 package com.app.kafka.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.app.kafka.processor.KafkaProducer;
 
 @RestController
 @RequestMapping("/rest/")
 public class KafkaResource {
 	
-	@GetMapping("/notification")
-	public void apiRequests() {
+	@Autowired
+	KafkaProducer kafkaProducer;
+	
+	@GetMapping("/message")
+	public void apiRequests(@RequestParam("input") String input) {
 		
-		
+		System.out.println("Sending input to the Topic");
+		kafkaProducer.sendOnTopic(input);
 		
 	}
 
